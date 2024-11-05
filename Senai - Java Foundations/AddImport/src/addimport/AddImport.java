6e43ef0b11c0c0607df331e38c6b320be20eeb28
Samuel Garcia <140754839+Muelgia@users.noreply.github.com>
2024-11-05 08:41:37 -0300

    Add files via upload

---
+++ Senai - Java Foundations/AddImport/src/addimport/AddImport.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package addimport;
+
+import java.util.*;
+import javax.swing.*;
+
+public class AddImport {
+
+    public static void main(String args[]) {
+        JLabel label = new JLabel("hello");
+
+    }
+
+}
